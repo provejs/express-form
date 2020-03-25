dea13e3f31aa7186dbb29b69c6a6ca224162e802
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2020-03-25 10:28:53 -0500

    Updated test

--- test/sanitize/toFloat.js
+++ test/sanitize/toFloat.js
@@ -5,7 +5,17 @@ var form = require('../../index');
 var field = form.field;
 
 describe('sanitize.toFloat()', function () {
-	it('should handle float string inputs', function () {
+	it('should handle string integer inputs', function () {
+		var req = {
+			body: {
+				field: '50'
+			}
+		};
+		form(field('field').toFloat())(req, {});
+		assert.ok(typeof req.form.field === 'number');
+		assert.strictEqual(req.form.field, 50);
+	});
+	it('should handle string float inputs', function () {
 		var req = {
 			body: {
 				field: '50.01'
@@ -13,7 +23,27 @@ describe('sanitize.toFloat()', function () {
 		};
 		form(field('field').toFloat())(req, {});
 		assert.ok(typeof req.form.field === 'number');
-		assert.equal(req.form.field, 50.01);
+		assert.strictEqual(req.form.field, 50.01);
+	});
+	it('should handle number inputs', function () {
+		var req = {
+			body: {
+				field: 123
+			}
+		};
+		form(field('field').toFloat())(req, {});
+		assert.ok(typeof req.form.field === 'number');
+		assert.strictEqual(req.form.field, 123);
+	});
+	it('should handle number 0 inputs', function () {
+		var req = {
+			body: {
+				field: 0
+			}
+		};
+		form(field('field').toFloat())(req, {});
+		assert.ok(typeof req.form.field === 'number');
+		assert.strictEqual(req.form.field, 0);
 	});
 	it('should handle non-float string inputs', function () {
 		var req = {
@@ -23,6 +53,50 @@ describe('sanitize.toFloat()', function () {
 		};
 		form(field('field').toFloat())(req, {});
 		assert.ok(typeof req.form.field === 'number');
+		assert.ok(isNaN(req.form.field));
+	});
+	it('should handle empty string inputs', function () {
+		var req = {
+			body: {
+				field: ''
+			}
+		};
+		form(field('field').toFloat())(req, {});
+		assert.ok(isNaN(req.form.field));
+	});
+	it('should handle undefined inputs', function () {
+		var req = {
+			body: {
+			}
+		};
+		form(field('field').toFloat())(req, {});
+		assert.ok(isNaN(req.form.field));
+	});
+	it('should handle null inputs', function () {
+		var req = {
+			body: {
+				field: null
+			}
+		};
+		form(field('field').toFloat())(req, {});
+		assert.ok(isNaN(req.form.field));
+	});
+	it('should handle true inputs', function () {
+		var req = {
+			body: {
+				field: true
+			}
+		};
+		form(field('field').toFloat())(req, {});
+		assert.ok(isNaN(req.form.field));
+	});
+	it('should handle false inputs', function () {
+		var req = {
+			body: {
+				field: false
+			}
+		};
+		form(field('field').toFloat())(req, {});
 		assert.ok(isNaN(req.form.field));
 	});
 });
